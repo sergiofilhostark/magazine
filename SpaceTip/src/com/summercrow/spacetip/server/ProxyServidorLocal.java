0ef6b60fe102fb511eeb4a0b13c97070190fa88b
sergiofilhostark <sergiofilho@gmail.com>
2013-08-18 17:45:36 -0300

    a

---
+++ SpaceTip/src/com/summercrow/spacetip/server/ProxyServidorLocal.java
@@ -0,0 +1,13 @@
+package com.summercrow.spacetip.server;
+
+import com.summercrow.spacetip.ProxyClienteLocal;
+
+public class ProxyServidorLocal {
+	
+	private ProxyClienteLocal proxyCliente;
+	
+	public ProxyServidorLocal(ProxyClienteLocal proxyCliente) {
+		this.proxyCliente = proxyCliente;
+	}
+
+}
