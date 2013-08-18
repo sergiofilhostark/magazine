a8c1564f45c4074a79d867e4c81392f8c1975b08
sergiofilhostark <sergiofilho@gmail.com>
2013-08-18 17:29:38 -0300

    m

--- SpaceTip/src/com/summercrow/spacetip/server/Partida.java
+++ SpaceTip/src/com/summercrow/spacetip/server/Partida.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.summercrow.spacetip.to.Resposta;
+
 public class Partida {
 	
 	private List<Jogador> jogadores;
